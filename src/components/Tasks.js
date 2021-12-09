d7e2fd525d6eacb2faa9000c541aadb0fd12656f
Gustavo Blas Duran <gustavoblasduran1999@gmail.com>
2021-12-08 23:26:42 -0600

    Delete and update elements in array

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -3,7 +3,13 @@ import Task from './Task'
 import PropTypes from 'prop-types'
 class Tasks extends Component {
     render() {
-        return this.props.tasks.map(item => <Task task={item} key={item.id}/>) 
+        return this.props.tasks.map(item => 
+        <Task 
+            task={item} 
+            key={item.id}
+            deleteTask={this.props.deleteTask}
+            checkDone={this.props.checkDone}
+        />) 
     }
 }
 
