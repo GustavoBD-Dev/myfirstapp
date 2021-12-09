490411f9eb22384360b195597654cefcb9c1938a
Gustavo Blas Duran <gustavoblasduran1999@gmail.com>
2021-12-08 19:36:53 -0600

    get and display JSON file items, tasks component lists the tasks

--- src/App.js
+++ src/App.js
@@ -1,49 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+// import the file with data 
+import tasks from './sample/tasks.json';
+import Tasks from './components/Tasks';
 
-/* function HelloWorld(props) {
-  return (
-    <div id="hello">{props.mytext}<h3>{props.subtitle}</h3></div>
-  )
-} */
-
-class HelloWorld extends React.Component {
+class App extends Component {
 
   state = {
-    show : true
+    tasks: tasks
   }
 
-  toggleShow = () => {
-    this.setState({show: !this.state.show})
-  }
-
-  render() {
-    if (this.state.show) {
-      return (
-        <div id="hello">
-          <h3>{this.props.mytext}</h3>
-          <h3>{this.props.subtitle}</h3>
-          <button onClick={this.toggleShow}>Toggle show</button>
-        </div>
-      )
-    } else {
-      return <h1>Thre are not elements
-        <button onClick={this.toggleShow}>Toggle show</button>
-      </h1>
-    }
-  }
-}
-
-
-function App() {
-  return (
-    <div>
-      This is my component: 
-      <HelloWorld mytext="Task 1" subtitle="component one"/> 
-      <HelloWorld mytext="Task 2" subtitle="component two"/> 
-      <HelloWorld mytext="Task 3" subtitle="component three"/> 
+  render () {
+    return <div>
+      <Tasks tasks={this.state.tasks}/>
     </div>
-  );
+  }
 }
 
 export default App;
