cc8bf33e09c119733d6cf7cf9f7d5851a481d281
Gustavo Blas Duran <gustavoblasduran1999@gmail.com>
2021-12-08 19:08:18 -0600

    state of components

--- src/App.js
+++ src/App.js
@@ -1,20 +1,47 @@
 import React from 'react';
 import './App.css';
 
-function HelloWorld(props) {
+/* function HelloWorld(props) {
   return (
     <div id="hello">{props.mytext}<h3>{props.subtitle}</h3></div>
   )
+} */
+
+class HelloWorld extends React.Component {
+
+  state = {
+    show : true
+  }
+
+  toggleShow = () => {
+    this.setState({show: !this.state.show})
+  }
+
+  render() {
+    if (this.state.show) {
+      return (
+        <div id="hello">
+          <h3>{this.props.mytext}</h3>
+          <h3>{this.props.subtitle}</h3>
+          <button onClick={this.toggleShow}>Toggle show</button>
+        </div>
+      )
+    } else {
+      return <h1>Thre are not elements
+        <button onClick={this.toggleShow}>Toggle show</button>
+      </h1>
+    }
+  }
 }
+
 
 function App() {
   return (
     <div>
       This is my component: 
-      <HelloWorld mytext="Hello Gus"/> 
-      <HelloWorld mytext="This is a example of props" subtitle="component two"/> 
-      {/* the component does not add the props*/}
-      <HelloWorld/> 
+      <HelloWorld mytext="Task 1" subtitle="component one"/> 
+      <HelloWorld mytext="Task 2" subtitle="component two"/> 
+      <HelloWorld mytext="Task 3" subtitle="component three"/> 
     </div>
   );
 }
