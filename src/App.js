bfd314443b1161e10a1c9a3805057432bb818f0a
Gustavo Blas Duran <gustavoblasduran1999@gmail.com>
2021-12-08 18:55:00 -0600

    Add props to component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import './App.css';
 
-function HelloWorld() {
+function HelloWorld(props) {
   return (
-    <div id="hello">Hello World</div>
+    <div id="hello">{props.mytext}<h3>{props.subtitle}</h3></div>
   )
 }
 
 function App() {
   return (
     <div>
-      This is my component: <HelloWorld/> <HelloWorld/>
+      This is my component: 
+      <HelloWorld mytext="Hello Gus"/> 
+      <HelloWorld mytext="This is a example of props" subtitle="component two"/> 
+      {/* the component does not add the props*/}
+      <HelloWorld/> 
     </div>
   );
 }
